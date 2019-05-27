9df9be13f7c9820cd57b29871ee13a2ccaf9f896
Paul Yang <palyang777@gmail.com>
2019-05-26 20:57:00 -0700

    finished project

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
 from flask import Flask, render_template, redirect, request, url_for
-from db import MongoDB, DBProxy
+from db import MongoDB, DBProxy, json
 
-uri = "mongodb+srv://dbUser:dbPassword@hack-roles-rvxnw.mongodb.net/test?retryWrites=true"
+uri = "mongodb+srv://dbUser:dbPassword@hack-roles-rvxnw.mongodb.net/test?retryWrites=true&ssl_cert_reqs=CERT_NONE"
 
-# https://cloud.mongodb.com/v2/5cc7e3fdc56c98c27c6a7fc8#clusters/detail/hack-roles
  
 app = Flask(__name__)
+
+
 db = MongoDB(uri)
-proxy_db = DBProxy("roles.json")
+# proxy_db = DBProxy("roles.json")
 
 
 @app.route('/', methods = ["GET", "POST"])
 def index():   
-        # return render_template("index.html", info_list = db["roles-info"].find())
-        return render_template("index.html", info_list = proxy_db.items())
+    processed_data = []
+    for item in db["roles-info"].find():
+        processed_data.append(item)
 
+    for item in processed_data:
+        item["qualifications"] = item["qualifications"][1:-1].split(",")
+        item["prompt"] = item["prompt"][1:-1].split(",")
+
+    # processed_data = proxy_db.items()
+
+    return render_template("index.html", info_list = processed_data)
+ 
 @app.route('/add', methods = ["GET", "POST"])
 def add_entry(): 
-        if request.method == "POST":
-            # db["roles-info"].insert_one(dict(request.form))
-            proxy_db.create(dict(request.form))
-            return redirect(url_for("index"))
- 
-        return render_template("add_entry.html")
+    if request.method == "POST":
+        db["roles-info"].insert_one(dict(request.form))
+        # proxy_db.create(dict(request.form))
+        return redirect(url_for("index"))
+
+    return render_template("add_entry.html")
 
 if __name__ == '__main__':  
     app.run(debug = True) 
